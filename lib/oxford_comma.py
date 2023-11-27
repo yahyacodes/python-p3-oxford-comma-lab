b4041dacaf6d8e9d63ec5356ea8baadc45b25267
yahyacodes <yahya.codin@gmail.com>
2023-11-27 14:24:52 +0300

    Completed assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 0:
+        return ''
+    elif len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f'{items[0]} and {items[1]}'
+    else:
+        comma = ', '.join(items[:-1])
+        return f'{comma}, and {items[-1]}'
